ca50ed78f726b055610010ea631b5161b04a55c9
Gent Semaj <gentsemaj@live.com>
2020-04-26 15:05:05 -0400

    Finalization 1
    - Completed asynchronous data retrieval process
    - Completed Weekly XP widget
    - Added loading spinner

--- scripting.js
+++ scripting.js
@@ -1,12 +1,13 @@
 var duoAuth;
-var userData;
+var competitors = ['OctavianDo4', 'GentSemaj', 'Konnor703548', 'blaisebot'];
+var userdata = [];
 
 $(document).ready(function(){
   // TestBoyMan
   // Testaccount
   $.ajax({
     type: 'POST',
-    url: "https://www.duolingo.com/login",
+    url: "https://cors-anywhere.herokuapp.com/https://www.duolingo.com/login",
     data: {"login": "TestBoyMan", "password": "Testaccount"},
     success: function(data, textStatus, request){
         var headers = request.getAllResponseHeaders().split("\n");
@@ -14,30 +15,112 @@ $(document).ready(function(){
         for(i = 0; i < headers.length; i++) {
           if(headers[i].startsWith("jwt")) {
             duoAuth = headers[i].substring(5);
-            requestData(data.username);
           }
+        }
+
+        if(duoAuth !=  null) {
+          console.log("Got jwt");
+          requestData(0);
+        } else {
+          alert("Failed to access Duolingo API");
         }
     }
  });
-
 });
 
-function requestData(username) {
-  if(duoAuth != null) {
-    /* TODO: figure out why we can't retrieve data */
+function requestData(index) {
+  if(index < competitors.length) {
+    var username = competitors[index];
     $.ajax({
       type: 'GET',
-      crossDomain: true,
-      dataType: "json",
-      headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET, OPTIONS", "Access-Control-Allow-Headers": "*", "Authorization": "Bearer " + duoAuth},
-      cookie: document.cookie,
-      url: "https://www.duolingo.com/users/" + username,
+      cache: false,
+      headers: {'Authorization': "Bearer " + duoAuth},
+      url: "https://cors-anywhere.herokuapp.com/https://www.duolingo.com/users/" + username,
       success: function(data, textStatus, request){
-        var resp = JSON.parse(data);
+        if(request.status == 200) {
+          userdata[index] = data;
+          console.log("User " + index + " is loaded");
+          requestData(index + 1);
+          /* This is done recursively to "synchronously"
+          load all user data. Using the actual async property
+          causes the page to freeze. */
+        }
       }
     });
-
   } else {
-    alert("Failed to access Duolingo API");
+    duoInit();
   }
+}
+
+function getXP(index) {
+  return Object.values(userdata[index].language_data)[0].points;
+}
+
+function getLastSunday() {
+  var date = new Date(Date.now()); // get current date
+  // subtract days until we hit the last sunday
+  while(date.getDay() > 0) {
+    date.setDate(date.getDate() - 1);
+  }
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+
+  return date;
+}
+
+function getWeeklyXP(index) {
+  var sum = 0;
+  var sunday = getLastSunday();
+  var cal = userdata[index].calendar;
+  for(e = 0; e < cal.length; e++) {
+    if(cal[e].datetime > sunday.getTime()) {
+      sum += cal[e].improvement;
+    }
+  }
+
+  return sum;
+}
+
+function getAvi(index) {
+  return "https://" + userdata[index].avatar.substring(2) + "/medium";
+}
+
+function duoInit() {
+
+  $("#xp-graph").hide();
+
+  console.log("Initializing");
+
+  /* Remove spinners */
+  $(".spinner").animate({
+    opacity: 0
+  }, 500, function(){
+    $(".spinner").remove();
+
+    /* Update widgets */
+
+    // XP bar graph
+    $("#xp-graph").show();
+    var i;
+    var pMax = 0; // maximum point value
+    for(i = 0; i < userdata.length; i++) {
+      var points = getWeeklyXP(i);
+      if(points > pMax) {
+        pMax = points;
+      }
+    }
+
+    var j;
+    for(j = 0; j < userdata.length; j++) {
+      var pct = getWeeklyXP(j) * 0.98 / pMax;
+      var barID = "bar-" + competitors[j];
+      $("#xp-graph").append("<tr><td class='avi'><img src='" + getAvi(j) + "' /></td><td id='" + barID + "'class='xp-bar'>" + getWeeklyXP(j) + " XP</td></tr>");
+      $("#" + barID).animate({
+        width: pct*100 + "%",
+        opacity: 1
+      }, 2000);
+    }
+  });
+
 }
